2a550b6d37f1a110346d43a3e1ac3ea4f46f9210
Fadhilah12 <stnurfahilah0113@gmail.com>
2025-07-08 06:59:45 +0700

    push

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,49 +1,33 @@
 //Navbar fixed
 window.onscroll = function () {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
 
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    }
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+  } else {
+    header.classList.remove('navbar-fixed');
+  }
 };
+//navbar header
+window.addEventListener('scroll', function () {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.remove('bg-transparent');
+    navbar.classList.add('bg-blue-600');
+  } else {
+    navbar.classList.remove('bg-blue-600');
+    navbar.classList.add('bg-transparent');
+  }
+});
 
 // hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
 hamburger.addEventListener('click', function () {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
 }
 );
-
-//testimoni
-let currentSlide = 0;
-
-  function moveSlide(step) {
-    const slider = document.getElementById("testimonial-slider");
-    const slides = slider.children;
-    const isMobile = window.innerWidth < 1024;
-
-    const visibleSlides = isMobile ? 1 : 3;
-    const totalPages = Math.ceil(slides.length / visibleSlides);
-
-    currentSlide += step;
-
-    if (currentSlide >= totalPages) {
-      currentSlide = 0;
-    } else if (currentSlide < 0) {
-      currentSlide = totalPages - 1;
-    }
-
-    const container = slider.parentElement;
-    const containerWidth = container.offsetWidth;
-    const translateX = currentSlide * containerWidth;
-
-    slider.style.transform = `translateX(-${translateX}px)`;
-  }
-
-  window.addEventListener("resize", () => moveSlide(0));
+;
